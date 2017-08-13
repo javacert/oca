ac1ebe8233ce1a7ea1b63e36d179f010e96d0f59
David Bingham <david.r.bingham@gmail.com>
2017-08-13 20:50:29 +0100

    Minor Correction

--- src/javaexamples/cert/scope/FinalKeywordTrickExamples.java
+++ src/javaexamples/cert/scope/FinalKeywordTrickExamples.java
@@ -4,10 +4,10 @@ public class FinalKeywordTrickExamples {
 
     final protected int someFinalVariableA = 4;
     protected final int someFinalVariableB = 4;
-    // protected int final someFinalVariableC = 4; // --> return type must appear immediately before method
+    // protected int final someFinalVariableC = 4; // --> return type must appear immediately before data type
 
     // private final Object contents; // --> Having final here is an issue:
-    // 1) It is not initialized
+    // 1) It may not have been initialized
     // 2) There is a setter for a final variable and this is not compatible
     private Object contents;
 
@@ -16,6 +16,6 @@ public class FinalKeywordTrickExamples {
     }
 
     public void setContents(Object someObjectToSet) {
-        this.setContents(someObjectToSet);
+        this.contents = someObjectToSet;
     }
 }
